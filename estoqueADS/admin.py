b86b36bf6902c06eca7279b42a0c503b24f55efe
KAUAN ALVES GODOIS <kauan.godois@alunos.sc.senac.br>
2024-04-25 21:47:26 -0300

    commit

--- estoqueADS/admin.py
+++ estoqueADS/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import Produtos
+from .models import Produtos, Categoria
 
 class adminProdutos(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'preco', 'quantidade', 'em_estoque']
+    list_display = ['id', 'nome', 'preco', 'quantidade', 'em_estoque', 'categoria']
     list_filter = ['em_estoque']
     search_fields = ['nome']
     list_editable = ['quantidade']
     list_per_page = 5
 
 admin.site.register(Produtos, adminProdutos)
+admin.site.register(Categoria)
 
 
 # Register your models here.
--- estoqueADS/views.py
+++ estoqueADS/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Produtos, Categoria
+from datetime import datetime
+ 
 def index(request):
-    frutas = ['Maçã', 'Uva', 'Jambo']
-    return render(request, 'pages/index.html', {"frutas": frutas})
+    produtos = Produtos.objects.all()
+    return render(request, 'pages/index.html', {'produtos': produtos})
+ 
+def adicionar_produto(request):
+   
+    if request.method == "POST":
+        nome = request.POST['nome']
+        categoria = request.POST['categoria']
+        preco = request.POST['preco']
+        descricao = request.POST['descricao']
+        quantidade = request.POST['quantidade'] 
+        codigo = request.POST['codigo']
+        em_estoque = False
+       
+        if int (quantidade) > 0:
+            em_estoque = True
+        data_criacao = datetime.now()
+
+        Produtos.objects.create(
+            nome=nome,
+            categoria_id=categoria,
+            preco=preco,
+            descricao=descricao,
+            quantidade=quantidade,
+            codigo=codigo,
+            em_estoque=em_estoque,
+            data_criacao=data_criacao
+        )
+
+        return redirect('index')
+
+    else:
+        categorias = Categoria.objects.all()
+        return render(request, 'pages/adicionar_produto.html', {'categoria': categorias})
+
+def produto(request, id):
+    detalhe = Produtos.objects.get(id=id)
+    return render(request, 'pages/produto.html', {'detalhe': detalhe})
+
+def excluir(request, id):
+    produto = Produtos.objects.get(id=id).delete()
+    return redirect('index')
